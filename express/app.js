f93367dd3db1d9b7e5e6e08109777b6adc81a1c0
Weten-Ogen <alphaoware@gmail.com>
2022-12-23 19:22:08 +0000

    worked on the express servers

--- express/app.js
+++ express/app.js
@@ -1,9 +1,31 @@
-const http = require('http')
+const express = require('express');
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1> hello this is the home page</h2>')
-        res.end()
-    }
-)
-server.listen(5000)
+
+const app = express()
+
+
+// app.use
+
+app.use(express.static('./public'))
+// app.get
+app.get('/',(req,res) =>{
+    res.sendFile(path.resolve(__dirname, './navbar/index.html'))
+})
+
+app.get('/about', (req,res) =>{
+    res.status(200).send('about page')
+})
+
+// app.listen
+// app.put
+// app.delete
+// app.all
+app.all("*",(req,res) =>{
+    res.status(404).send("<h1>the resource doesn't exit or you entered a wrong url </h1>")
+})
+// app.post
+
+app.listen(5000, () => {
+    console.log('the server is listenning ... ')
+})
